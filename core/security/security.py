1dd467c057e47906d3faa5b73d6dc9c5292606b0
Pablo <apollo.zero@outlook.com>
2026-04-17 10:48:41 -0300

    feat: was added full login system with refresh token, only missing google auth

--- core/security/security.py
+++ core/security/security.py
@@ -1,85 +1,120 @@
-import jwt
 from datetime import datetime, timedelta, timezone
+
+import asyncpg
+import jwt
+import redis
+from fastapi import Depends, HTTPException, Request
+from services.cache import cache_service
+
+from core.config.config import settings, COOKIE_AUTH_REFRESH
+from core.config.config import COOKIE_AUTH, COOKIE_AUTH_RESET, ROLE_RANK_BY_NAME
 from core.logger.logger import logger
-from core.config.config import settings
 from core.postgresql.postgresql import postgresql
+from core.redis.redis_cache import redis_cache
 from services.user import user_service
-from fastapi import Request, HTTPException, Depends
-from google.oauth2 import id_token
-from google.auth.transport import requests
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
+def decode_access_token(token: str) -> dict:
+    return jwt.decode(
+        token,
+        settings.SECRET_KEY,
+        algorithms=[settings.ALGORITHM],
+    )
 
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-    return encoded_jwt
+
+def create_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    payload = data.copy()
+    expire = datetime.now(timezone.utc) + (
+            expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    payload.update({"exp": expire})
+
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
-def decode_access_token(token: str) -> dict | None:
+async def verify_token(
+        token: str,
+        conn: asyncpg.Connection,
+        redis_client: redis.Redis,
+        check_can_update: bool = False,
+        expected_type: str = "auth",
+) -> dict | bool | None:
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        return payload
-    except jwt.ExpiredSignatureError:
-        logger.error("Token has expired")
-        return None
-    except jwt.InvalidTokenError:
-        logger.error("Invalid token")
-        return None
-
-
-def verify_google_token(token: str) -> dict | None:
-    try:
-        user = id_token.verify_oauth2_token(token, requests.Request(), settings.GOOGLE_CLIENT_ID)
-
-        return {**user}
-    except ValueError as e:
-        logger.error(e)
-        return None
-
-
-async def verify_token(token: str, conn) -> dict | bool | None:
-    try:
-
         if token.startswith("Bearer "):
             token = token[7:]
 
         payload = decode_access_token(token)
 
-        if payload["userId"]:
-            response = await user_service.get_one_user(conn, payload["userId"])
+        if payload.get("type") != expected_type:
+            raise jwt.InvalidTokenError("Token type mismatch")
 
-            if response["status"] is None or not response["status"]:
-                raise jwt.InvalidSignatureError("User not found")
+        if expected_type == "reset" and not check_can_update:
+            if payload.get("canUpdate") is not False:
+                raise jwt.InvalidTokenError("Invalid reset token stage")
 
-            return dict(response["data"]["user"])
-        else:
+        if not payload.get("userId"):
             raise jwt.InvalidTokenError("Invalid token payload")
 
-    # I use None to represent expired, and False to invalid *
+        response = await user_service.get_one_user(conn, payload["userId"])
+
+        if response["status"] is None or not response["status"]:
+            raise jwt.InvalidSignatureError("User not found")
+
+        if check_can_update:
+            if payload.get("canUpdate"):
+                return dict(response["data"]["user"])
+            raise jwt.InvalidTokenError("User does not have update permissions")
+
+        requires_session = expected_type in ("auth", "refresh")
+
+        if requires_session:
+            session = await cache_service.get_by_key(
+                f"session:{payload['sessionId']}", redis_client
+            )
+            if not session:
+                raise jwt.InvalidTokenError("Session is invalid")
+
+        return dict(response["data"]["user"])
+
     except jwt.ExpiredSignatureError:
         logger.error("Token has expired")
         return None
     except jwt.InvalidTokenError:
-        logger.error(f"Invalid token")
+        logger.error("Invalid token")
         return False
 
 
-async def validate_token(request: Request, conn) -> dict:
+async def validate_token(
+        request: Request,
+        conn: asyncpg.Connection,
+        redis_client: redis.Redis,
+        check_can_update: bool = False,
+        reset_cookie: bool = False,
+        refresh_cookie: bool = False,
+        expected_type: str = "auth",
+) -> dict:
     try:
-        cookie_key = "auth"
+        cookie_key = (
+            COOKIE_AUTH_RESET
+            if reset_cookie
+            else COOKIE_AUTH_REFRESH if refresh_cookie else COOKIE_AUTH
+        )
         token = request.cookies.get(cookie_key)
 
         if not token:
             raise HTTPException(status_code=401, detail="Not authenticated")
 
-        user = await verify_token(token, conn=conn)
+        user = await verify_token(
+            token,
+            conn=conn,
+            check_can_update=check_can_update,
+            expected_type=expected_type,
+            redis_client=redis_client,
+        )
 
-        # I use None to represent expired, and False to invalid **
         if user is None:
             raise HTTPException(status_code=401, detail="Token has expired")
+
         if not user:
             raise HTTPException(status_code=401, detail="Invalid token")
 
@@ -87,34 +122,80 @@ async def validate_token(request: Request, conn) -> dict:
 
         return user
 
+    except HTTPException:
+        raise
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-async def validate_token_wrapper(request: Request, conn=Depends(postgresql.get_db)) -> dict:
-    return await validate_token(request, conn)
+async def validate_token_to_update_password(
+        request: Request,
+        conn: asyncpg.Connection = Depends(postgresql.get_db),
+        redis_client: redis.Redis = Depends(redis_cache.get_redis),
+) -> dict:
+    return await validate_token(
+        request,
+        conn,
+        check_can_update=True,
+        reset_cookie=True,
+        refresh_cookie=False,
+        expected_type="reset",
+        redis_client=redis_client,
+    )
 
 
-# ==========================================================
-# JUST IN CASE IN THE FUTURE I NEED TO CONTROL ROLE ACCESS
-# ==========================================================
-
-def require_minimum_rank(minimum_rank: int, user: dict):
-
-    role_ranks = {
-        "BASIC": 1,
-        "ADMIN": 2
-    }
-
-    user_role = user.get("role", "").upper()
-    rank = role_ranks.get(user_role, 0)
-
-    if rank < minimum_rank:
-        raise HTTPException(status_code=403, detail="User doesn't have enough rank")
-
-    return user
+async def validate_token_to_validate_code(
+        request: Request,
+        conn: asyncpg.Connection = Depends(postgresql.get_db),
+        redis_client: redis.Redis = Depends(redis_cache.get_redis),
+) -> dict:
+    return await validate_token(
+        request,
+        conn,
+        check_can_update=False,
+        reset_cookie=True,
+        refresh_cookie=False,
+        expected_type="reset",
+        redis_client=redis_client,
+    )
 
 
-async def require_admin_rank(user = Depends(validate_token_wrapper)):
-    return require_minimum_rank(2, user)
+async def validate_token_refresh(
+        request: Request,
+        conn: asyncpg.Connection = Depends(postgresql.get_db),
+        redis_client: redis.Redis = Depends(redis_cache.get_redis),
+) -> dict:
+    return await validate_token(
+        request,
+        conn,
+        check_can_update=False,
+        reset_cookie=False,
+        refresh_cookie=True,
+        expected_type="refresh",
+        redis_client=redis_client,
+    )
+
+
+async def validate_token_wrapper(
+        request: Request,
+        conn: asyncpg.Connection = Depends(postgresql.get_db),
+        redis_client: redis.Redis = Depends(redis_cache.get_redis),
+) -> dict:
+    return await validate_token(request, conn, redis_client)
+
+
+def require_minimum_rank(minimum_rank: int):
+    async def dependency(user: dict = Depends(validate_token_wrapper)) -> dict:
+        rank = ROLE_RANK_BY_NAME.get(user.get("role", "").upper(), 0)
+
+        if rank < minimum_rank:
+            raise HTTPException(status_code=403, detail="Insufficient permissions")
+
+        return user
+
+    return dependency
+
+
+def require_admin_rank():
+    return require_minimum_rank(2)
