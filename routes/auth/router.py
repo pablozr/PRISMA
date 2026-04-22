b74c3af57727b780659f98479ec9c359457531e9
Pablo <apollo.zero@outlook.com>
2026-04-22 12:03:46 -0300

    feat: was implemented refresh token route

--- routes/auth/router.py
+++ routes/auth/router.py
@@ -5,14 +5,14 @@ from starlette.responses import JSONResponse
 
 from core.security.rate_limit import LOGIN_RATE_LIMIT_DEPS
 from schemas.auth.auth import UserLoginRequest
-from services.auth.auth_service import login, logout
+from services.auth.auth_service import login as auth_login, logout as auth_logout, refresh_token as auth_refresh
 
 router = APIRouter()
 
 
 @router.post("/login", dependencies=LOGIN_RATE_LIMIT_DEPS)
 async def login(conn: asyncpg.Connection, redis_client: redis.Redis, data: UserLoginRequest):
-    response = await login(conn, redis_client, data)
+    response = await auth_login(conn, redis_client, data)
 
     if not response["status"]:
         return JSONResponse(status_code=400, content={"detail": response["message"]})
@@ -50,7 +50,7 @@ async def logout(request: Request, redis_client: redis.Redis):
     if not session_id:
         return JSONResponse(status_code=400, content={"detail": "Session ID cookie is missing"})
 
-    response = await logout(redis_client, session_id)
+    response = await auth_logout(redis_client, session_id)
 
     if not response["status"]:
         return JSONResponse(status_code=400, content={"detail": response["message"]})
@@ -62,4 +62,35 @@ async def logout(request: Request, redis_client: redis.Redis):
     return resp
 
 
+@router.post("/refresh", dependencies=LOGIN_RATE_LIMIT_DEPS)
+async def refresh_token(request: Request, redis_client: redis.Redis):
+    response = await auth_refresh(redis_client, request.state.token)
 
+    if not response["status"]:
+        return JSONResponse(status_code=400, content={"detail": response["message"]})
+
+    access_token = response["data"].pop("accessToken")
+    refresh_token = response["data"].pop("refreshToken")
+
+    resp = JSONResponse(status_code=200, content={"message": response["message"], "data": response["data"]})
+    resp.set_cookie(
+        key="COOKIE_AUTH",
+        value=access_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        path="/",
+        max_age=900
+    )
+
+    resp.set_cookie(
+        key="COOKIE_REFRESH",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        path="/",
+        max_age=604800,
+    )
+
+    return resp
