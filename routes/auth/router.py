84781a3e52f8d7e333296a6f22ef777c0c897500
Pablo <apollo.zero@outlook.com>
2026-04-22 11:45:20 -0300

    feat: was implemented logout route

---
+++ routes/auth/router.py
@@ -0,0 +1,65 @@
+import asyncpg
+import redis
+from fastapi import APIRouter, Request
+from starlette.responses import JSONResponse
+
+from core.security.rate_limit import LOGIN_RATE_LIMIT_DEPS
+from schemas.auth.auth import UserLoginRequest
+from services.auth.auth_service import login, logout
+
+router = APIRouter()
+
+
+@router.post("/login", dependencies=LOGIN_RATE_LIMIT_DEPS)
+async def login(conn: asyncpg.Connection, redis_client: redis.Redis, data: UserLoginRequest):
+    response = await login(conn, redis_client, data)
+
+    if not response["status"]:
+        return JSONResponse(status_code=400, content={"detail": response["message"]})
+
+    access_token = response["data"].pop("accessToken")
+    refresh_token = response["data"].pop("refreshToken")
+
+    resp = JSONResponse(status_code=200, content={"message": response["message"], "data": response["data"]})
+    resp.set_cookie(
+        key="COOKIE_AUTH",
+        value=access_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        path="/",
+        max_age=900
+    )
+
+    resp.set_cookie(
+        key="COOKIE_REFRESH",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        path="/",
+        max_age=604800,
+    )
+
+    return resp
+
+
+@router.post("/logout")
+async def logout(request: Request, redis_client: redis.Redis):
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        return JSONResponse(status_code=400, content={"detail": "Session ID cookie is missing"})
+
+    response = await logout(redis_client, session_id)
+
+    if not response["status"]:
+        return JSONResponse(status_code=400, content={"detail": response["message"]})
+
+    resp = JSONResponse(status_code=200, content={"message": response["message"]})
+    resp.delete_cookie("COOKIE_AUTH", path="/", samesite="lax")
+    resp.delete_cookie("COOKIE_REFRESH", path="/", samesite="lax")
+
+    return resp
+
+
+
